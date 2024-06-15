dc37a027128c723b6d905c8f0c71a3cf294645a7
MAHROUS-ACR <mahrous.ref@gmail.com>
2024-06-15 22:13:06 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCz-ydE1F9Gfm8_d6exEfq2hdSuZmQRSd4",
+    authDomain: "bckacr-49e4c.firebaseapp.com",
+    databaseURL: "https://bckacr-49e4c-default-rtdb.firebaseio.com",
+    projectId: "bckacr-49e4c",
+    storageBucket: "bckacr-49e4c.appspot.com",
+    messagingSenderId: "646102338822",
+    appId: "1:646102338822:web:a6de8a05e59aff3c736287",
+    measurementId: "G-2MTRQT4H5M"
+  };
+
